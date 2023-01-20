78ccf721c4a635d250a7e14f8ff2a521685cce10
Ofek Zohar <ofekzohar1@mail.tau.ac.il>
2023-01-20 23:21:14 +0200

    v2.0 - final sol

--- pcc_client.c
+++ pcc_client.c
@@ -1,107 +1,157 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
-#include <netdb.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
 #include <arpa/inet.h>
-#include <errno.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 
+//************************* Const Macros *************************************
+
 #define VALID_NUM_ARGS 4
 #define BUFF_SIZE 1048576   // 2^20B = 1 MB
+#define FAILURE (-1)
+#define SUCCESS 0
 
-int main(int argc, char *argv[])
-{
-    in_port_t server_port;
-    in_addr_t server_ip;
-    uint32_t C, N;
-    int sockfd;
-    ssize_t bytes_cnt, bytes_tot, bytes_read_from_file, bytes_sent;
+//************************* Helper Functions *****************************
 
+// Parse user input into server IP/port
+// return 0 on success, -1 on failure
+int parse_user_input(int argc, char *argv[], in_addr_t *server_ip, in_port_t *server_port) {
     if (argc != VALID_NUM_ARGS) {
         fprintf(stderr, "The program should get %u arguments: %s.\n", VALID_NUM_ARGS - 1, strerror(EINVAL));
-        exit(EXIT_FAILURE);
+        return FAILURE;
     }
-    server_port = (in_port_t) atoi(argv[2]);
-    if (inet_pton(AF_INET, argv[1], &server_ip) != 1) {
+    *server_port = (in_port_t) atoi(argv[2]);
+    if (inet_pton(AF_INET, argv[1], server_ip) != 1) {
         fprintf(stderr, "Invalid IP address: %s.\n", strerror(errno));
-        exit(EXIT_FAILURE);
+        return FAILURE;
     }
+    return SUCCESS;
+}
 
-    int fd = open(argv[3], O_RDONLY);
-    if (fd < 0) {
-        fprintf(stderr, "Error opening file %s: %s.\n", argv[3], strerror(errno));
-        exit(EXIT_FAILURE);
+// Open file and determine its size
+// return file fd on success, -1 on failure
+int open_and_get_file_size(char *filepath, uint32_t *file_size) {
+    int filefd = open(filepath, O_RDONLY); // Open for read only
+    if (filefd < 0) {
+        fprintf(stderr, "Error opening file %s: %s.\n", filepath, strerror(errno));
+        return FAILURE;
     }
 
     struct stat st;
-    fstat(fd, &st);
-    N = st.st_size;
+    if (fstat(filefd, &st) != 0) { // Get file's stat
+        fprintf(stderr, "Error get file size (%s): %s.\n", filepath, strerror(errno));
+        close(filefd);
+        return FAILURE;
+    }
+    *file_size = st.st_size;
+
+    return filefd;
+}
+
+// Connect to the server
+// return new socket fd on success, -1 on failure
+int connect_to_server(in_addr_t server_ip, in_port_t server_port) {
+    int sockfd;
+    // Server Attributes. Using htonl/s to get network byte order
+    struct sockaddr_in serv_addr;
+    socklen_t addrsize = sizeof(struct sockaddr_in);
+    memset(&serv_addr, 0, addrsize);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(server_port);
+    serv_addr.sin_addr.s_addr = server_ip;
+
+    if ((sockfd = socket( AF_INET, SOCK_STREAM, 0)) < 0) { // New IPv4/TCP socket
+        fprintf(stderr, "Create socket Failed: %s.\n", strerror(errno));
+        return FAILURE;
+    }
+
+    if(connect(sockfd,(struct sockaddr*) &serv_addr,addrsize) < 0) { // Connect socket to server
+        fprintf(stderr, "Connect failed: %s.\n", strerror(errno));
+        return FAILURE;
+    }
+
+    return sockfd;
+}
+
+// Send file data to the sever using TCP
+// C: # of printable chars sent to the server, calculate at the server side
+// return C on success, -1 on failure
+ssize_t send_file_to_server(int sockfd, int filefd, uint32_t N, int C_size) {
+    ssize_t bytes_cnt, bytes_tot, bytes_read_from_file, bytes_sent;
     char buff[BUFF_SIZE];
     memset(buff, 0 ,sizeof(buff));
 
-    struct sockaddr_in serv_addr;
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(server_port); // Note: htons for endiannes
-    serv_addr.sin_addr.s_addr = server_ip;
-    socklen_t addrsize = sizeof(struct sockaddr_in );
-
-    if ((sockfd = socket( AF_INET, SOCK_STREAM, 0)) < 0) {
-        fprintf(stderr, "Create socket Failed: %s.\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-
-    if(connect(sockfd,(struct sockaddr*) &serv_addr,addrsize) < 0) {
-        fprintf(stderr, "Connect failed: %s.\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
     ((uint32_t *)buff)[0] = htonl(N);
-    bytes_tot = 0;
-    while (bytes_tot < sizeof(N)) {
+
+    bytes_tot = 0; // Count the total bytes sent
+    while (bytes_tot < sizeof(N)) { // Send N to the server - # of bytes to be sent
         if((bytes_cnt = write(sockfd, buff + bytes_tot, sizeof(N) - bytes_tot)) < 0) {
             fprintf(stderr, "Send N failed: %s.\n", strerror(errno));
-            exit(EXIT_FAILURE);
+            return FAILURE;
         }
         bytes_tot += bytes_cnt;
     }
 
     bytes_tot = bytes_read_from_file = 0;
-    while(bytes_tot < N)
-    {
-        if (bytes_read_from_file == 0) {
-            if ((bytes_read_from_file = read(fd, buff, BUFF_SIZE)) < 0) {
-                fprintf(stderr, "Read from file %s failed: %s.\n", argv[3], strerror(errno));
-                exit(EXIT_FAILURE);
+    while(bytes_tot < N) { // Send N bytes to the server
+        if (bytes_read_from_file == 0) { // Read from file to buffer
+            if ((bytes_read_from_file = read(filefd, buff, BUFF_SIZE)) < 0) {
+                fprintf(stderr, "Read from file failed: %s.\n", strerror(errno));
+                return FAILURE;
             }
             bytes_sent = 0;
         }
+        // Send buffer to the server
         if((bytes_cnt = write(sockfd, buff + bytes_sent, bytes_read_from_file)) < 0) {
             fprintf(stderr, "Send bytes over TCP failed: %s.\n", strerror(errno));
-            exit(EXIT_FAILURE);
+            return FAILURE;
         }
         bytes_read_from_file -= bytes_cnt;
         bytes_sent += bytes_cnt;
         bytes_tot += bytes_cnt;
     }
-    close(fd);
 
     bytes_tot = 0;
-    while (bytes_tot < sizeof(C)) {
-        if((bytes_cnt = read(sockfd, buff + bytes_tot, sizeof(C) - bytes_tot)) < 0) {
+    while (bytes_tot < C_size) { // Read C from the server - # of printable chars sent
+        if((bytes_cnt = read(sockfd, buff + bytes_tot, C_size - bytes_tot)) < 0) {
             fprintf(stderr, "Receive bytes over TCP failed: %s.\n", strerror(errno));
-            exit(EXIT_FAILURE);
+            return FAILURE;
         }
         bytes_tot += bytes_cnt;
     }
-    close(sockfd);
 
-    C = ntohl(((uint32_t *)buff)[0]);
+    return ntohl(((uint32_t *)buff)[0]); // Return C in host byte order
+}
+
+//********************************* Main *************************************
+
+int main(int argc, char *argv[])
+{
+    in_port_t server_port; // Server port to be used
+    in_addr_t server_ip; // Server IP to be used
+    uint32_t C, N;
+    int sockfd, filefd;
+
+    if (parse_user_input(argc, argv, &server_ip, &server_port) == FAILURE) // Parse user input
+        exit(EXIT_FAILURE);
+
+    filefd = open_and_get_file_size(argv[3], &N); // Open file nad get its size
+    if (filefd == FAILURE) exit(EXIT_FAILURE);
+
+    sockfd = connect_to_server(server_ip, server_port); // Open TCP connection to server
+    if(sockfd == FAILURE) exit(EXIT_FAILURE);
+
+    C = send_file_to_server(sockfd, filefd, N, sizeof(C)); // Send file to server
+    close(filefd);
+    close(sockfd);
+    if (C == FAILURE) exit(EXIT_FAILURE);
+
     printf("# of printable characters: %u\n", C);
-    return 0;
+    return EXIT_SUCCESS;
 }
--- pcc_server.c
+++ pcc_server.c
@@ -8,16 +8,24 @@
 #include <sys/types.h>
 #include <signal.h>
 
+//************************* Const Macros *************************************
+
 #define VALID_NUM_ARGS 2
 #define MIN_PRINTABLE_CHAR 32
 #define MAX_PRINTABLE_CHAR 126
 #define LISTEN_QUEUE_SIZE 10
 #define BUFF_SIZE 1048576   // 2^20B = 1 MB
 #define TCP_ERROR (errno == ETIMEDOUT || errno == ECONNRESET || errno == EPIPE)
+#define FAILURE (-1)
+#define SUCCESS 0
+#define CONTINUE (-2)
 
-static uint32_t pcc_total[MAX_PRINTABLE_CHAR - MIN_PRINTABLE_CHAR + 1];
-static int connfd, sigint_sent;
+static uint32_t pcc_total[MAX_PRINTABLE_CHAR - MIN_PRINTABLE_CHAR + 1]; // Count all the printable chars the server processed
+static int connfd, sigint_sent; // connfd: connection socket, -1 means no connection, sigint_sent: is SIGINT sent to the process
 
+//************************* SIGINT & Server Exit *****************************
+
+// Safe exit the server process, printing all printable chars the server processed
 void exit_server() {
     for (int i = 0; i < MAX_PRINTABLE_CHAR - MIN_PRINTABLE_CHAR + 1; i++) {
         printf("char '%c' : %u times\n", i + MIN_PRINTABLE_CHAR, pcc_total[i]);
@@ -25,48 +33,46 @@ void exit_server() {
     exit(EXIT_SUCCESS);
 }
 
+// SIGINT costume handler
 void sigint_handler() {
-    if (connfd < 0) {
+    if (connfd < 0) { // If no active connection, exit the server
         exit_server();
     } else {
-        sigint_sent = 1;
+        sigint_sent = 1; // Active connection, finish processing that connection
     }
 }
 
 const struct sigaction sigint = {.sa_handler = &sigint_handler, .sa_flags = SA_RESTART};
 
-int main(int argc, char *argv[])
-{
-    int one = 1;
-    in_port_t server_port;
-    ssize_t bytes_cnt, bytes_tot, i;
-    uint32_t N, C;
-    int tcp_err, listenfd = -1;
-    connfd = -1, sigint_sent = 0;
+//************************* Helper Functions *****************************
 
+// Parse user input into server_port
+// return 0 on success, -1 on failure
+int parse_user_input(int argc, char *argv[], in_port_t *server_port) {
     if (argc != VALID_NUM_ARGS) {
         fprintf(stderr, "The program should get %u arguments: %s.\n", VALID_NUM_ARGS - 1, strerror(EINVAL));
-        exit(EXIT_FAILURE);
+        return FAILURE;
     }
-    server_port = (in_port_t) atoi(argv[1]);
+    *server_port = (in_port_t) atoi(argv[1]);
+    return SUCCESS;
+}
 
-    char buff[BUFF_SIZE];
-    uint32_t pcc_counter[MAX_PRINTABLE_CHAR - MIN_PRINTABLE_CHAR + 1];
+// Open new listen socket with queue size == LISTEN_QUEUE_SIZE
+// return new listen socket fd on success, -1 on failure
+int open_listen_socket(in_port_t server_port) {
+    int listenfd;
 
-    if (sigaction(SIGINT, &sigint, 0) != 0) {
-        fprintf(stderr, "Error setting SIGINT handler: %s.\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-
-    if ((listenfd = socket( AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((listenfd = socket( AF_INET, SOCK_STREAM, 0)) < 0) { // New IPv4/TCP socket
         fprintf(stderr, "Socket Failed: %s.\n", strerror(errno));
-        exit(EXIT_FAILURE);
+        return FAILURE;
     }
-    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int)) != 0) {
+    // Set listen socket to be 'SO_REUSEADDR', so the address could be reuse
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) != 0) {
         fprintf(stderr, "Set socket option Failed: %s.\n", strerror(errno));
-        exit(EXIT_FAILURE);
+        return FAILURE;
     }
 
+    // Server Attributes. Using htonl/s to get network byte order
     struct sockaddr_in serv_addr;
     socklen_t addrsize = sizeof(struct sockaddr_in);
     memset(&serv_addr, 0, addrsize);
@@ -74,93 +80,118 @@ int main(int argc, char *argv[])
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); // INADDR_ANY = any local machine address
     serv_addr.sin_port = htons(server_port);
 
-    if(bind(listenfd,(struct sockaddr*) &serv_addr,addrsize) != 0)
+    if(bind(listenfd,(struct sockaddr*) &serv_addr,addrsize) != 0) // Bind socket to port
     {
         fprintf(stderr, "Bind Failed: %s.\n", strerror(errno));
-        exit(EXIT_FAILURE);
+        return FAILURE;
     }
-    if(listen(listenfd, LISTEN_QUEUE_SIZE) != 0)
+    if(listen(listenfd, LISTEN_QUEUE_SIZE) != 0) // Start listen on server port
     {
         fprintf(stderr,"Listen Failed: %s.\n", strerror(errno));
+        return FAILURE;
+    }
+
+    return listenfd;
+}
+
+// Accept and process new connections
+// return 0 on success, -1 on failure other than TCP, -2 on TCP errors
+int accept_new_connection(int listenfd, char *buff, uint32_t *pcc_counter) {
+    ssize_t bytes_cnt, bytes_tot, i;
+    uint32_t N, C = 0;
+
+    if ((connfd = accept(listenfd, NULL, NULL)) < 0) { // Wait for new connection
+        fprintf(stderr, "Accept Failed: %s.\n", strerror(errno));
+        return FAILURE;
+    }
+
+    bytes_tot = 0; // Count the total bytes sent
+    while (bytes_tot < sizeof(N)) { // Read N from the client - # of bytes to be sent
+        if ((bytes_cnt = read(connfd, buff + bytes_tot, sizeof(N) - bytes_tot)) <= 0) {
+            if ((bytes_cnt == 0 && bytes_tot < sizeof(N)) || TCP_ERROR) { // 0 means client stopped unexpectedly, TCP errors
+                fprintf(stderr, "TCP error (N): %s.\nAccepting new connections.\n", strerror(errno));
+                return CONTINUE;
+            } else if (bytes_cnt < 0) { // Any other error
+                fprintf(stderr, "Receive N over TCP failed: %s.\n", strerror(errno));
+                return FAILURE;
+            }
+        }
+        bytes_tot += bytes_cnt;
+    }
+    N = ntohl(((uint32_t *) buff)[0]); // Parse N from the buffer in host byte order
+
+    bytes_tot = 0;
+    while (bytes_tot < N) { // Read N bytes from the client
+        if ((bytes_cnt = read(connfd, buff, BUFF_SIZE)) <= 0) {
+            if ((bytes_cnt == 0 && bytes_tot < N) || TCP_ERROR) {
+                fprintf(stderr, "TCP error (data): %s.\nAccepting new connections.\n", strerror(errno));
+                return CONTINUE;
+            } else if (bytes_cnt < 0) {
+                fprintf(stderr, "Receive data over TCP failed: %s.\n", strerror(errno));
+                return FAILURE;
+            }
+        }
+
+        for (i = 0; i < bytes_cnt; i++) { // Count the printable chars sent
+            if (buff[i] < MIN_PRINTABLE_CHAR || buff[i] > MAX_PRINTABLE_CHAR) continue;
+            pcc_counter[buff[i] - MIN_PRINTABLE_CHAR]++; // Count per char
+            C++; // Count all printable chars
+        }
+        bytes_tot += bytes_cnt;
+    }
+
+    ((uint32_t *) buff)[0] = htonl(C); // Put C (# of printable chars) in buffer (network byte order)
+    bytes_tot = 0;
+    while (bytes_tot < sizeof(C)) { // Send C to the client
+        if ((bytes_cnt = write(connfd, buff + bytes_tot, sizeof(C) - bytes_tot)) <= 0) {
+            if ((bytes_cnt == 0 && bytes_tot < sizeof(C)) || TCP_ERROR) {
+                fprintf(stderr, "TCP error (C): %s.\nAccepting new connections.", strerror(errno));
+                return CONTINUE;
+            } else if (bytes_cnt < 0) {
+                fprintf(stderr, "Send C over TCP failed: %s.\n", strerror(errno));
+                return FAILURE;
+            }
+        }
+        bytes_tot += bytes_cnt;
+    }
+
+    return SUCCESS;
+}
+
+//********************************* Main *************************************
+
+int main(int argc, char *argv[])
+{
+    in_port_t server_port; // Server port to be used
+    int ret, listenfd;
+    connfd = -1, sigint_sent = 0;
+
+    if (parse_user_input(argc, argv, &server_port) == FAILURE) // Parse user input
+        exit(EXIT_FAILURE);
+
+    if (sigaction(SIGINT, &sigint, 0) != 0) { // Set SIGINT new handler
+        fprintf(stderr, "Error setting SIGINT handler: %s.\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    while (!sigint_sent) {
-        if ((connfd = accept(listenfd, NULL, NULL)) < 0) {
-            fprintf(stderr,"Accept Failed: %s.\n", strerror(errno));
-            exit(EXIT_FAILURE);
-        }
-        C = tcp_err = 0;
-        memset(&pcc_counter, 0, sizeof(pcc_counter));
+    listenfd = open_listen_socket(server_port); // Open listen socket
+    if (listenfd == FAILURE) exit(EXIT_FAILURE);
 
-        bytes_tot = 0;
-        while (bytes_tot < sizeof(N)) {
-            if((bytes_cnt = read(connfd, buff + bytes_tot, sizeof(N) - bytes_tot)) <= 0) {
-                if ((bytes_cnt == 0 && bytes_tot < sizeof(N)) || TCP_ERROR) {
-                    fprintf(stderr, "TCP error (N): %s.\nAccepting new connections.", strerror(errno));
-                    close(connfd);
-                    connfd = -1;
-                    tcp_err = 1;
-                    break;
-                } else if (bytes_cnt < 0) {
-                    fprintf(stderr, "Receive N over TCP failed: %s.\n", strerror(errno));
-                    exit(EXIT_FAILURE);
-                }
-            }
-            bytes_tot += bytes_cnt;
-        }
-        if (tcp_err) continue;
-        N = ntohl(((uint32_t *)buff)[0]);
+    char buff[BUFF_SIZE];
+    uint32_t pcc_counter[MAX_PRINTABLE_CHAR - MIN_PRINTABLE_CHAR + 1]; // Local counter
+    while (!sigint_sent) { // Accept connections to the server until SIGINT
+        connfd = -1; // No active connection
+        memset(&pcc_counter, 0, sizeof(pcc_counter)); // Clear the counter
+        ret = accept_new_connection(listenfd, buff, pcc_counter);
+        close(connfd);
 
-        bytes_tot = 0;
-        while(bytes_tot < N)
-        {
-            if((bytes_cnt = read(connfd, buff, N - bytes_tot)) <= 0) {
-                if ((bytes_cnt == 0 && bytes_tot < N) || TCP_ERROR) {
-                    fprintf(stderr, "TCP error (data): %s.\nAccepting new connections.", strerror(errno));
-                    close(connfd);
-                    connfd = -1;
-                    tcp_err = 1;
-                    break;
-                } else if (bytes_cnt < 0) {
-                    fprintf(stderr, "Receive data over TCP failed: %s.\n", strerror(errno));
-                    exit(EXIT_FAILURE);
-                }
-            }
+        if (ret == FAILURE) exit(EXIT_FAILURE); // Unexpected error
+        if (ret == CONTINUE) continue; // TCP error, no need to update pcc_total
 
-            for (i = 0; i < bytes_cnt; i++) {
-                if (buff[i] < MIN_PRINTABLE_CHAR || buff[i] > MAX_PRINTABLE_CHAR) continue;
-                pcc_counter[buff[i] - MIN_PRINTABLE_CHAR]++;
-                C++;
-            }
-            bytes_tot += bytes_cnt;
-        }
-        if (tcp_err) continue;
-
-        ((uint32_t *)buff)[0] = htonl(C);
-        bytes_tot = 0;
-        while (bytes_tot < sizeof(C)) {
-            if((bytes_cnt = write(connfd, buff + bytes_tot, sizeof(C) - bytes_tot)) <= 0) {
-                if ((bytes_cnt == 0 && bytes_tot < sizeof(C)) || TCP_ERROR) {
-                    fprintf(stderr, "TCP error (C): %s.\nAccepting new connections.", strerror(errno));
-                    close(connfd);
-                    connfd = -1;
-                    tcp_err = 1;
-                    break;
-                } else if (bytes_cnt < 0) {
-                    fprintf(stderr, "Send C over TCP failed: %s.\n", strerror(errno));
-                    exit(EXIT_FAILURE);
-                }
-            }
-            bytes_tot += bytes_cnt;
-        }
-        if (tcp_err) continue;
-
-        for (i = 0; i < MAX_PRINTABLE_CHAR - MIN_PRINTABLE_CHAR + 1; i++) {
+        // Update the global counter according to the local one
+        for (int i = 0; i < MAX_PRINTABLE_CHAR - MIN_PRINTABLE_CHAR + 1; i++) {
             pcc_total[i] += pcc_counter[i];
         }
-        close(connfd);
-        connfd = -1;
     }
     exit_server();
 }
